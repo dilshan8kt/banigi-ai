38da50a07b475c7eab7d6b194bb4369d24759dcd
Tharindu Dilshan <dilshan.kt@gmail.com>
2024-05-28 22:14:10 +0530

    key update

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = "https://api.reimaginehome.ai";
-export const API_KEY = "66530f6dbfb09206e0f67369";
+export const API_KEY = "6588ble82e6242419bf5";
 export const SUPABASE_URL = "https://hlqksxtmdjdlptzebzkj.supabase.co";
 export const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhscWtzeHRtZGpkbHB0emViemtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MDc4NzMsImV4cCI6MjAzMjQ4Mzg3M30.LLJQ9UsI71SycLHkqEDsGhHfb__isGO1UEf0SfYZPco";
