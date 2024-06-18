3086b075f98707fa8c1292433240a686e76c4430
Malan Amarasinghe <85899461+MalanDev@users.noreply.github.com>
2024-06-18 13:31:28 +0530

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://104.248.110.55:4000";
+export const API_BASE_URL = "https://api.reimaginehome.ai";
 export const API_KEY = "666690bc6b283d58d9bbf43c";
 export const SUPABASE_URL = "https://hlqksxtmdjdlptzebzkj.supabase.co";
 export const SUPABASE_ANON_KEY =
@@ -6,4 +6,4 @@ export const SUPABASE_ANON_KEY =
 export const SUPABASE_BUCKET_PATH =
   "https://hlqksxtmdjdlptzebzkj.supabase.co/storage/v1/object/public/banigi-ai%20images/";
 
-export const BACKEND_API_URL = "https://bangi-ai-backend.onrender.com";
+export const BACKEND_API_URL = "http://104.248.110.55:4000";
