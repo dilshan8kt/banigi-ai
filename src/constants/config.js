8b51c9c2547277190924bdb885ee846a9ff3f3d8
imalshakalan <imalsha773@gmail.com>
2024-06-18 14:41:21 +0530

    api key

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = "https://api.reimaginehome.ai";
-export const API_KEY = "666690bc6b283d58d9bbf43c";
+export const API_KEY = "66714df1cf9d93674f0496cb";
 export const SUPABASE_URL = "https://hlqksxtmdjdlptzebzkj.supabase.co";
 export const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhscWtzeHRtZGpkbHB0emViemtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MDc4NzMsImV4cCI6MjAzMjQ4Mzg3M30.LLJQ9UsI71SycLHkqEDsGhHfb__isGO1UEf0SfYZPco";
